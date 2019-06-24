561eb5ff01b9e338536ff2831a525a31b6a45263
uliana.stefanishyna <uliana.stefanishyna@globallogic.com>
2019-06-24 13:01:24 +0300

    wrote eureka server and client

---
+++ eureka-client/src/main/java/com/spring/cloud/demo/model/HelloObject.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.demo.model;
+
+public class HelloObject {
+  private String message;
+
+  public String getMessage() {
+      return message;
+  }
+
+  public void setMessage(String message) {
+      this.message = message;
+  }
+}
